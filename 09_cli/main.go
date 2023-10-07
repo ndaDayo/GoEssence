fea6bcd881f48ea4ed163b10a48ea03707800958
ndaDayo <hayato711+nda@gmail.com>
2023-10-07 13:21:38 +0900

    add: findEntry

--- 09_cli/main.go
+++ 09_cli/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
+	"path"
 	"regexp"
 	"strings"
 
@@ -36,36 +38,62 @@ func findEntires(siteURL string) ([]Entry, error) {
 		return nil, err
 	}
 	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
+	entries := []Entry{}
 	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
 		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
 		if len(token) != 3 {
 			return
 		}
 
+		title := elem.Text()
 		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html",
 			token[1], token[2])
 		author, zipURL := findAuthorAndZIP(pageURL)
-		println(zipURL)
+		if zipURL != "" {
+			entries = append(entries, Entry{
+				AuthorID: token[1],
+				Author:   author,
+				TitleID:  token[2],
+				Title:    title,
+				SiteURL:  siteURL,
+				ZipURL:   zipURL,
+			})
+		}
 	})
 
 	return nil, nil
 }
 
 func findAuthorAndZIP(siteURL string) (string, string) {
+	log.Println("query", siteURL)
 	doc, err := goquery.NewDocument(siteURL)
-
 	if err != nil {
 		return "", ""
 	}
 
 	author := doc.Find("table[summary=作家データ] tr:nth-child(2) td:nth-child(2)").First().Text()
 	zipURL := ""
-
 	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
 		href := elem.AttrOr("href", "")
 		if strings.HasSuffix(href, ".zip") {
 			zipURL = href
 		}
 	})
+
+	if zipURL == "" {
+		return author, ""
+	}
+
+	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "http://") {
+		return author, zipURL
+	}
+
+	u, err := url.Parse(siteURL)
+
+	if err != nil {
+		return author, ""
+	}
+
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
 	return author, zipURL
 }
