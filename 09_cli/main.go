9565a8ee46390535897af06a774f856d2ed3fdc2
ndaDayo <hayato711+nda@gmail.com>
2023-10-07 14:00:57 +0900

    add: extractText

--- 09_cli/main.go
+++ 09_cli/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
 	"path"
 	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/text/encoding/japanese"
 )
 
 type Entry struct {
@@ -28,7 +34,13 @@ func main() {
 		log.Fatal(err)
 	}
 	for _, entry := range entries {
+		content, err := extractText(entry.ZipURL)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 		fmt.Printf(entry.Title, entry.ZipURL)
+		fmt.Println(content)
 	}
 }
 
@@ -61,7 +73,7 @@ func findEntires(siteURL string) ([]Entry, error) {
 		}
 	})
 
-	return nil, nil
+	return entries, nil
 }
 
 func findAuthorAndZIP(siteURL string) (string, string) {
@@ -96,4 +108,41 @@ func findAuthorAndZIP(siteURL string) (string, string) {
 
 	u.Path = path.Join(path.Dir(u.Path), zipURL)
 	return author, zipURL
+}
+
+func extractText(zipURL string) (string, error) {
+	resp, err := http.Get(zipURL)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	r, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
+
+	for _, file := range r.File {
+		if path.Ext(file.Name) == ".txt" {
+			f, err := file.Open()
+			if err != nil {
+				return "", err
+			}
+			b, err := ioutil.ReadAll(f)
+			f.Close()
+			if err != nil {
+				return "", err
+			}
+			b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
+			if err != nil {
+				return "", err
+			}
+
+			return string(b), nil
+		}
+	}
+
+	return "", errors.New("contents not found")
 }
