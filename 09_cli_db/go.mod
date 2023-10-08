ee489b06b335eea111b1ffb143ea4e78c0436726
ndaDayo <hayato711+nda@gmail.com>
2023-10-08 21:22:49 +0900

    add: setupDB, addEntry

--- 09_cli_db/go.mod
+++ 09_cli_db/go.mod
@@ -3,10 +3,13 @@ module cli_db
 go 1.20
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/ikawaha/kagome v1.11.2 // indirect
 	github.com/ikawaha/kagome-dict v1.0.9 // indirect
 	github.com/ikawaha/kagome-dict/ipa v1.0.10 // indirect
 	github.com/ikawaha/kagome/v2 v2.9.4 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
--- 09_cli_db/main.go
+++ 09_cli_db/main.go
@@ -1,25 +1,175 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"database/sql"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"net/http"
+	"net/url"
+	"path"
+	"regexp"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/ikawaha/kagome-dict/ipa"
 	"github.com/ikawaha/kagome/v2/tokenizer"
 	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/text/encoding/japanese"
 )
 
+type Entry struct {
+	AuthorID string
+	Author   string
+	TitleID  string
+	Title    string
+	SiteURL  string
+	ZipURL   string
+}
+
 func main() {
-	db, err := sql.Open("sqlite3", "database.sqlite")
+	db, err := setupDB("database.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+
+	entries, err := findEntires(listURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer db.Close()
+	log.Printf("found %s entries", len(entries))
+
+	for _, entry := range entries {
+		log.Printf("adding %+v", entry)
+		content, err := extractText(entry.ZipURL)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		err = addEntry(db, &entry, content)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+}
+
+func findEntires(siteURL string) ([]Entry, error) {
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return nil, err
+	}
+	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
+	entries := []Entry{}
+	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+		if len(token) != 3 {
+			return
+		}
+
+		title := elem.Text()
+		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html",
+			token[1], token[2])
+		author, zipURL := findAuthorAndZIP(pageURL)
+		if zipURL != "" {
+			entries = append(entries, Entry{
+				AuthorID: token[1],
+				Author:   author,
+				TitleID:  token[2],
+				Title:    title,
+				SiteURL:  siteURL,
+				ZipURL:   zipURL,
+			})
+		}
+	})
+
+	return entries, nil
+}
+
+func findAuthorAndZIP(siteURL string) (string, string) {
+	log.Println("query", siteURL)
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return "", ""
+	}
+
+	author := doc.Find("table[summary=作家データ] tr:nth-child(2) td:nth-child(2)").First().Text()
+	zipURL := ""
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+
+	if zipURL == "" {
+		return author, ""
+	}
+
+	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "http://") {
+		return author, zipURL
+	}
+
+	u, err := url.Parse(siteURL)
+
+	if err != nil {
+		return author, ""
+	}
+
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
+	return author, zipURL
+}
+
+func extractText(zipURL string) (string, error) {
+	resp, err := http.Get(zipURL)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	r, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
+
+	for _, file := range r.File {
+		if path.Ext(file.Name) == ".txt" {
+			f, err := file.Open()
+			if err != nil {
+				return "", err
+			}
+			b, err := ioutil.ReadAll(f)
+			f.Close()
+			if err != nil {
+				return "", err
+			}
+			b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
+			if err != nil {
+				return "", err
+			}
+
+			return string(b), nil
+		}
+	}
+
+	return "", errors.New("contents not found")
+}
+
+func setupDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "database.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for _, query := range queries() {
 		_, err = db.Exec(query)
@@ -28,77 +178,8 @@ func main() {
 		}
 	}
 
-	b, err := os.ReadFile("ababababa.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	return db, nil
 
-	b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-	content := string(b)
-
-	res, err := db.Exec(`INSERT INTO authors(author_id, author) values(?, ?)`,
-		"000879",
-		"芥川竜之介",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	res, err = db.Exec(`INSERT INTO contents(author_id, title_id, title, content) values(?, ?, ?, ?)`,
-		"000879",
-		"14",
-		"あばばばば",
-		content,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	docID, err := res.LastInsertId()
-	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	seg := t.Wakati(content)
-
-	_, err = db.Exec(`INSERT INTO contents_fts(docid, words) values (?, ?)`, docID, strings.Join(seg, " "))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	query := "虫 AND ココア"
-	rows, err := db.Query(`
-        SELECT 
-            a.author,
-            c.title
-        FROM
-            contents c
-        INNER JOIN authors a
-            ON a.author_id = c.author_id
-        INNER JOIN contents_fts f
-            ON c.rowid = f.docid
-            AND words MATCH ?
-    `, query)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var author, title string
-		err = rows.Scan(&author, &title)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(author, title)
-	}
 }
 
 func queries() []string {
@@ -109,4 +190,48 @@ func queries() []string {
 	}
 
 	return queries
+}
+
+func addEntry(db *sql.DB, entry *Entry, content string) error {
+	_, err := db.Exec(`
+        REPLACE INTO authors(author_id, author) values(?, ?)
+    `,
+		entry.AuthorID,
+		entry.Author,
+	)
+
+	if err != nil {
+		return err
+	}
+	res, err := db.Exec(`
+        REPLACE INTO contents(author_id, title_id, title, conten) values(?, ?, ?, ?)
+    `,
+		entry.AuthorID,
+		entry.TitleID,
+		entry.Title,
+		content,
+	)
+
+	if err != nil {
+		return err
+	}
+	docID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+	if err != nil {
+		return err
+	}
+	seg := t.Wakati(content)
+	_, err = db.Exec(
+		`REPLACE INTO contents_fts(docid, words) values (?, ?)`,
+		docID,
+		strings.Join(seg, " "),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
